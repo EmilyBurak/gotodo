83d3267ff30939fae7b0084e4f900c3d5e27ba29
Emily T. Burak <110084300+EmilyBurak@users.noreply.github.com>
2024-12-01 09:57:29 -0700

    add reminders through eventbridge scheduler

--- cmd/remind.go
+++ cmd/remind.go
@@ -2,18 +2,21 @@ package cmd
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/lambda"
+	"github.com/aws/aws-sdk-go-v2/service/scheduler"
+	"github.com/aws/aws-sdk-go-v2/service/scheduler/types"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-func generateAWSScheduleExpression(hour string, minute string) string {
-	return "cron(" + minute + " " + hour + " * * ? *)"
-}
+// func generateAWSScheduleExpression(hour string, minute string) string {
+// 	return "cron(" + minute + " " + hour + " * * ? *)"
+// }
 
 var remindCmd = &cobra.Command{
 	Use:   "remind",
@@ -21,47 +24,51 @@ var remindCmd = &cobra.Command{
 	Long:  `Invoke a lambda function to remind you about a task`,
 	Run: func(cmd *cobra.Command, args []string) {
 
+		viper.SetConfigType("env")
+		viper.SetConfigName(".env")
+		viper.AddConfigPath(".")
+		err := viper.ReadInConfig()
+		if err != nil {
+			panic(fmt.Errorf("fatal error config file: %w", err))
+		}
+
 		task := cmd.Flag("task").Value.String()
 		message := cmd.Flag("message").Value.String()
 
 		hour := cmd.Flag("hour").Value.String()
 		minute := cmd.Flag("minute").Value.String()
 
-		// Generate the rate expression
-		scheduleExpression := generateAWSScheduleExpression(hour, minute)
+		today := time.Now()
+		formattedToday := today.Format("2006-01-02T")
 
-		// Create a new Lambda client
+		log.Printf("Task: %s, Message: %s, Hour: %s, Minute: %s", task, message, hour, minute)
+
 		cfg, err := config.LoadDefaultConfig(context.TODO())
 		if err != nil {
 			fmt.Println("Error loading configuration:", err)
 			return
 		}
 
-		client := lambda.NewFromConfig(cfg)
+		client := scheduler.NewFromConfig(cfg)
 
-		// Define the payload
-		payload, err := json.Marshal(map[string]string{
-			"task":     task,
-			"message":  message,
-			"schedule": scheduleExpression,
-		})
-
-		if err != nil {
-			fmt.Println("Error marshaling payload:", err)
-			return
-		}
-
-		// Invoke the Lambda function
-		_, err = client.Invoke(context.TODO(), &lambda.InvokeInput{
-			FunctionName: aws.String("goscheduler"),
-			Payload:      payload,
+		_, err = client.CreateSchedule(context.TODO(), &scheduler.CreateScheduleInput{
+			FlexibleTimeWindow: &types.FlexibleTimeWindow{Mode: types.FlexibleTimeWindowModeOff},
+			Name:               aws.String("reminderfor" + task),
+			// yyyy-mm-ddThh:mm:ss
+			ScheduleExpression: aws.String("at(" + formattedToday + hour + ":" + minute + ":00)"),
+			Target: &types.Target{
+				Arn:     aws.String(viper.Get("SNS_TOPIC_ARN").(string)),
+				RoleArn: aws.String(viper.Get("ROLE_ARN").(string)),
+				Input:   aws.String(fmt.Sprintf(`Task: %s, Message: %s`, task, message)),
+			},
+			ScheduleExpressionTimezone: aws.String("America/Denver"),
+			State:                      types.ScheduleStateEnabled,
 		})
 		if err != nil {
-			fmt.Println("Error invoking lambda function:", err)
+			fmt.Println("Error creating schedule:", err)
 			return
 		}
-
-		fmt.Println("Message sent to lambda function:" + string(payload))
+		fmt.Println("Reminder set for task: " + task)
 	},
 }
 
@@ -69,7 +76,9 @@ func init() {
 	rootCmd.AddCommand(remindCmd)
 	remindCmd.Flags().StringP("task", "t", "", "Task to remind you about")
 	remindCmd.MarkFlagRequired("task")
-	remindCmd.Flags().StringP("message", "m", "", "Message to send to the lambda function")
-	remindCmd.Flags().StringP("hour", "H", "12", "Hour to remind you about the task")
-	remindCmd.Flags().StringP("minute", "M", "0", "Minute to remind you about the task")
+	remindCmd.Flags().StringP("hour", "H", "12", "Hour to remind you about the task in 24-hour format")
+	remindCmd.MarkFlagRequired("hour")
+	remindCmd.Flags().StringP("minute", "M", "0", "Minute of hour to remind you about the task")
+	remindCmd.MarkFlagRequired("minute")
+	remindCmd.Flags().StringP("message", "m", "", "Message to send to AWS SNS")
 }
